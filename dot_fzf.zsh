beedeba60e0017e601aa1cd7eb9b08b2874468bf
Daniel Stoddart <stoddart@gmail.com>
2025-03-07 23:19:19 +0800

    Add new dotfiles to chezmoi

---
+++ dot_fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */Users/daniel/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/Users/daniel/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/Users/daniel/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/daniel/.fzf/shell/key-bindings.zsh"
